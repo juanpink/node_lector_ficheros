5d53c96e0cd63cc633c01ac3b267b23d22967eac
juanpink <jmiopin6@gmail.com>
2024-12-11 10:38:42 +0100

    Primer commit de prueba.

--- reader.js
+++ reader.js
@@ -3,6 +3,9 @@ import fs from 'node:fs';//Require package.json
 try {
     const data = fs.readFileSync('./quijote.txt', 'utf8');
     console.log(data);
+    let nombre = new String();
+    console.log(nombre);
+    console.log(typeof nombre);
 } catch (err) {
     console.error(err);
 }
